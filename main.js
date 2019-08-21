2b25f2953f1ca843a8c2f1d1100a3337125b6c5f
nicostran <nstranquist@gmail.com>
2019-08-20 23:13:55 -0500

    add project to new git repo

---
+++ main.js
@@ -0,0 +1,123 @@
+// get DOM objects
+const menuBars = document.querySelector('.menu-toggler');
+const topNav = document.querySelector('.top-nav');
+const upBtn = document.getElementById('up');
+const navLinks = document.querySelectorAll('.nav-link');
+
+// toggle nav menu
+menuBars.addEventListener('click', () => {
+  menuBars.classList.toggle('open');
+  topNav.classList.toggle('open');
+})
+// close nav menu when link is clicked
+topNav.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target && e.target.tagName === 'A') {
+    menuBars.classList.remove('open');
+    topNav.classList.remove('open');
+  }
+});
+
+
+// page scroll WITHOUT jQuery
+function scrollIt(destination, duration = 200, easing = 'linear', callback) {
+
+  const easings = {
+    linear(t) {
+      return t;
+    },
+    easeInQuad(t) {
+      return t * t;
+    },
+    easeOutQuad(t) {
+      return t * (2 - t);
+    },
+    easeInOutQuad(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    },
+    easeInCubic(t) {
+      return t * t * t;
+    },
+    easeOutCubic(t) {
+      return (--t) * t * t + 1;
+    },
+    easeInOutCubic(t) {
+      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    },
+    easeInQuart(t) {
+      return t * t * t * t;
+    },
+    easeOutQuart(t) {
+      return 1 - (--t) * t * t * t;
+    },
+    easeInOutQuart(t) {
+      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+    },
+    easeInQuint(t) {
+      return t * t * t * t * t;
+    },
+    easeOutQuint(t) {
+      return 1 + (--t) * t * t * t * t;
+    },
+    easeInOutQuint(t) {
+      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+    }
+  };
+
+  const start = window.pageYOffset;
+  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+  const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+  const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+  const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+  if ('requestAnimationFrame' in window === false) {
+    window.scroll(0, destinationOffsetToScroll);
+    if (callback) {
+      callback();
+    }
+    return;
+  }
+
+  function scroll() {
+    const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+    const time = Math.min(1, ((now - startTime) / duration));
+    const timeFunction = easings[easing](time);
+    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+    if (window.pageYOffset === destinationOffsetToScroll) {
+      if (callback) {
+        callback();
+      }
+      return;
+    }
+
+    requestAnimationFrame(scroll);
+  }
+
+  scroll();
+}
+
+// scroll page to top when up button is clicked
+upBtn.addEventListener('click', () => scrollIt(
+  0,
+  1000));
+
+// scroll to section when its .nav-link is clicked
+for (let a = 0; a < navLinks.length; a++) {
+  navLinks[a].addEventListener('click', () => {
+    let linkDestination = document.querySelector(`${navLinks[a].getAttribute('href')}`);
+    scrollIt(
+      linkDestination.offsetTop,
+      1000
+    );
+  });
+}
+
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+  easing: 'ease',
+  duration: 1800,
+  once: true  //sets so that animates only once per page load
+});
